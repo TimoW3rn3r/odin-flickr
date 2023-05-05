fd332222376f4185fbd216ed29d0c4d37f32ab66
Werner <turbotimoh@gmail.com>
2023-05-05 12:34:32 +0545

    feat: add `people.getPhotos` method to index uses flicker id from params to get photos from the Flicker API

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,12 @@
 class StaticPagesController < ApplicationController
   def index
+    @photos = []
+    if not params[:flickr_id]
+      return
+    end
+
+    flickr = ApplicationController::FLICKR
+    photos_info = flickr.people.getPhotos(user_id: params[:flickr_id])
+    @photos = photos_info.map { |photo| Flickr.url_z(photo) }
   end
 end
